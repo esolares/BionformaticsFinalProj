7c5d7fc5dc1388a5f41d4da24905b49993e4380b
esolares <solarese@uci.edu>
2017-03-24 17:29:21 -0700

    Update qsub_cactus_yeast.sh

--- qsub_cactus_yeast.sh
+++ qsub_cactus_yeast.sh
@@ -17,5 +17,5 @@ source /data/apps/progressiveCactus/environment
 #export PATH=${PATH}:/data/users/solarese/bin/hdf5-1.8.11/bin
 #export h5prefix=-prefix=/data/users/solarese/bin/hdf5-1.8.11
 
-runProgressiveCactus.sh --maxThreads 32 yeast.txt PCwork PCwork/yeast.hal
-hal2assemblyHub.py PCwork/yeast.hal AssHub --maxThreads 32 --lod
+runProgressiveCactus.sh --maxThreads 32 yeast.txt YeastPCwork YeastPCwork/yeast.hal
+hal2assemblyHub.py YeastPCwork/yeast.hal YeastHub --maxThreads 32 --lod
